375b4dfb4db52cee9fc1db684ac1430c1c369119
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2023-04-06 17:02:57 +0200

    repo components

---
+++ app/code/repos/[name]/page.jsx
@@ -0,0 +1,17 @@
+import Repo from "@/app/components/Repo";
+import Link from "next/link";
+import RepoDirs from "@/app/components/RepoDirs";
+
+const RepoPage = ({ params: { name } }) => {
+  return (
+    <div className="card">
+      <Link href="/code/repos" className="btn btn-back">
+        Back To Repositories
+      </Link>
+      <Repo name={name} />
+      <RepoDirs name={name} />
+    </div>
+  );
+};
+
+export default RepoPage;
