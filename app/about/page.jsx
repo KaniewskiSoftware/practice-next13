4d9b53ab443f7deb9f19b91c9424b2af8eec2f8d
st4rkmano <117393647+st4rkmano@users.noreply.github.com>
2023-04-06 16:09:59 +0200

    basic strucutre and layout

---
+++ app/about/page.jsx
@@ -0,0 +1,15 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla
+        accusamus quasi provident ipsa quis deserunt, magni officiis eum itaque
+        sunt qui eius tenetur obcaecati necessitatibus. Soluta quibusdam
+        exercitationem unde excepturi.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,15 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+        architecto beatae quae sit voluptatem iste asperiores suscipit corrupti
+        ex earum dolor, neque, voluptas deleniti! Temporibus delectus
+        perferendis assumenda culpa expedita!
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
